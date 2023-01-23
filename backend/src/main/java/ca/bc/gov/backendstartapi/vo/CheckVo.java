da81d988915b2ccbea7c9887b7d81dc47196e531
Ricardo Campos <ricardo.campos@encora.com>
2023-01-23 18:12:56 -0300

    feat: Remove check endpoint (#24)
    * feat: remove check endpoint and update to use actuator
    
    * ci: autogenerated JaCoCo coverage badge
    
    * feat: add db component status check to postman collection
    
    * ci: bump action-deployer-openshift to lastest version
    
    Co-authored-by: Ci Bot <cibot@users.noreply.github.com>
    Co-authored-by: Annibal Silva <105941480+annibalsilva@users.noreply.github.com>

--- backend/src/main/java/ca/bc/gov/backendstartapi/vo/CheckVo.java
+++
@@ -1,6 +0,0 @@
-package ca.bc.gov.backendstartapi.vo;
-
-import ca.bc.gov.backendstartapi.response.BaseResponse;
-
-/** This class represents a check object. */
-public record CheckVo(String message, String release) implements BaseResponse {}
