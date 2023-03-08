5364ca2e162fed95a3cd39489f119a69093a3a10
Ricardo Campos <ricardo.campos@encora.com>
2023-03-08 12:31:53 -0300

    chore: remove unused imports

--- backend/src/main/java/ca/bc/gov/backendstartapi/exception/UserNotFoundException.java
+++ backend/src/main/java/ca/bc/gov/backendstartapi/exception/UserNotFoundException.java
@@ -1,7 +1,6 @@
 package ca.bc.gov.backendstartapi.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.server.ResponseStatusException;
 
